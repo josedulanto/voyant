042577fb88e1a8fdeb0ea3183e1b085255bca26c
José Dulanto <jose@dulan.to>
2019-09-05 00:04:42 -0500

    added phoneparser PHONE command-line tool

--- tools.rb
+++ tools.rb
@@ -3,6 +3,7 @@
 require 'dotenv/load'
 require 'thor'
 require 'pry'
+require 'csv'
 
 class Tools < Thor
   desc 'checkpassword PASSWORD',
@@ -16,7 +17,8 @@ class Tools < Thor
     puts common_password ? 'Common password' : 'Not common password'
   end
 
-  desc 'weather ZIPCODE --country COUNTRY', 'finds the live weather for a zip code'
+  desc 'weather ZIPCODE --country COUNTRY',
+       'finds the live weather for a zip code'
   option :country, default: 'us', desc: 'country'
   def weather(zipcode)
     response = owm_api_for_zipcode(zipcode, country: options[:country])
@@ -31,6 +33,37 @@ class Tools < Thor
 
     puts msg
   end
+
+  # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+  desc 'phoneparser PHONE', 'Phone number parser'
+  def phoneparser(phone)
+    country = 'United States'
+    numbers = phone.gsub(/\D+/, '')
+    phone_number = ''
+    code = ''
+
+    if numbers.length > 10
+      first_three, remaining_numbers = numbers
+                                       .match(/^(.{0,3})(.{0,15})\s*$/)
+                                       .captures
+      csv = CSV.read('./countryCodes.csv')
+      3.times do |n|
+        try_code = first_three.slice(0, n + 1)
+        country, code = csv.detect { |row| row[1] == try_code }
+        next unless code
+
+        first_three.slice!(0, n + 1)
+        phone_number = first_three + remaining_numbers
+        break
+      end
+    else
+      code = '1'
+      phone_number = numbers
+    end
+
+    puts "#{country} +#{code}#{phone_number}"
+  end
+  # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
 
   private
 
